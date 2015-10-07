d333a75a47ce74fd06c0acce3746ec0e753d2c8d
yuki514kk <kksahiyuko.5148901@gmail.com>
2015-10-07 22:52:28 +0900

    マイナスボタン関数の追加

--- code.js
+++ code.js
@@ -1,17 +1,22 @@
 function plus() {
    mokuteki = document.getElementById("num");
    count = mokuteki.innerHTML;
-   aaa = parseInt(count);
-   if(aaa < 10) {
-	aaa++;
-	mokuteki.innerHTML = aaa;
+   now_count = parseInt(count);
+   if(now_count < 10) {
+	now_count++;
+	mokuteki.innerHTML = now_count;
    }
 
 }
 
 function minus(str) {
-
-
+   mokuteki = document.getElementById("num");
+   count = mokuteki.innerHTML;
+   now_count = parseInt(count);
+   if(now_count > 0) {
+	now_count--;
+	mokuteki.innerHTML = now_count;
+   }
 }
 
 function reset(str) {
