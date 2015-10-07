f8d676ea9cbd31bc8716338284712a96b4d2a03a
yuki514kk <kksahiyuko.5148901@gmail.com>
2015-10-07 22:38:07 +0900

    プラスボタン関数の追加

---
+++ code.js
@@ -0,0 +1,20 @@
+function plus() {
+   mokuteki = document.getElementById("num");
+   count = mokuteki.innerHTML;
+   aaa = parseInt(count);
+   if(aaa < 10) {
+	aaa++;
+	mokuteki.innerHTML = aaa;
+   }
+
+}
+
+function minus(str) {
+
+
+}
+
+function reset(str) {
+
+
+}
